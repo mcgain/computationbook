584e74728dd4207810217e85ce0f7ba20705bdd6
Richard McGain <richard.mcgain@shopify.com>
2019-09-13 13:45:04 +0930

    scopes

--- the_meaning_of_programs/mcgain_scope.rb
+++ the_meaning_of_programs/mcgain_scope.rb
@@ -1,6 +1,5 @@
 require_relative 'small_step'
 
-require'pry-byebug';binding.pry
 
 class Scope < Struct.new(:body)
   def to_s
@@ -11,23 +10,46 @@ class Scope < Struct.new(:body)
     "{{#{self}}}"
   end
 
+  def reducible?
+    true
+  end
+
   def reduce(environment)
-    [body, {}]
+    [body, Environment.new(environment)]
+  end
+end
+
+class Environment
+  def initialize(parent_environment)
+    @parent_environment = parent_environment
+    @this_environment = {}
+  end
+
+  def [](key)
+    @this_environment[key] || @parent_environment[key]
+  end
+
+  def []=(key, value)
+    @this_environment[key] = value
+  end
+
+  def merge(env)
+    @this_environment.merge!(env)
   end
 end
 
 mac = Machine.new(
   Sequence.new(
+    Assign.new(:x, Number.new(1)),
     Scope.new(
-      Sequence.new(
-        Assign.new(:x, Number.new(1)),
-        Add.new(Number.new(2), Variable.new(:x)))),
-    Scope.new(
-      If.new(Variable.new(:x),
+        If.new(LessThan.new(Variable.new(:x), Number.new(2)),
         Assign.new(:y, Number.new(1)),
         Assign.new(:y, Number.new(2)))
     )
   ), {}
 )
+
+mac.run
+require'pry-byebug';binding.pry
 
 true
